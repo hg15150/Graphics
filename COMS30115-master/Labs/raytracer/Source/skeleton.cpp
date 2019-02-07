e73bb356ba2f7c128c0b6816b76ebcd8c60af339
Harry Goode <hg15150@my.bristol.ac.uk>
2019-02-07 18:01:26 +0000

    Section 2 complete
    The intercept function works

--- COMS30115-master/Labs/raytracer/Source/skeleton.cpp
+++ COMS30115-master/Labs/raytracer/Source/skeleton.cpp
@@ -17,22 +17,39 @@ SDL_Event event;
 #define SCREEN_HEIGHT 256
 #define FULLSCREEN_MODE false
 
+struct Intersection
+  {
+  vec4 position;
+  float distance;
+  int triangleIndex;
+  };
+
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 bool Update();
 void Draw(screen* screen);
+bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection );
 
 int main( int argc, char* argv[] )
 {
-  
+
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
 
-  while ( Update())
-    {
-      Draw(screen);
-      SDL_Renderframe(screen);
-    }
+  vec4 start(0,0,0,1);
+  vec4 dir(1,0,0,1);
+
+  vector<Triangle> triangles;
+  LoadTestModel( triangles );
+  Intersection closestIntersection;
+
+  ClosestIntersection(start, dir, triangles, closestIntersection);
+
+  // while ( Update())
+  //   {
+  //     Draw(screen);
+  //     SDL_Renderframe(screen);
+  //   }
 
   SDL_SaveImage( screen, "screenshot.bmp" );
 
@@ -45,7 +62,7 @@ void Draw(screen* screen)
 {
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
+
   vec3 colour(1.0,0.0,0.0);
   for(int i=0; i<1000; i++)
     {
@@ -93,7 +110,40 @@ bool Update()
 		/* Move camera quit */
 		return false;
 	      }
-	  }  
+	  }
     }
   return true;
+}
+
+bool xChecker(vec3 x){
+  return ( (x.x >= 0) && (x.y >= 0) && (x.z >= 0) && ( (x.y + x.z) < 1) );
+}
+
+bool ClosestIntersection(vec4 s, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection ){
+
+  for (size_t i = 0; i < triangles.size(); i++) {
+    vec4 v0 = triangles[i].v0;
+    vec4 v1 = triangles[i].v1;
+    vec4 v2 = triangles[i].v2;
+
+    vec3 e1 = vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
+    vec3 e2 = vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
+    vec3 b = vec3(s.x-v0.x,s.y-v0.y,s.z-v0.z);
+
+    mat3 A( -vec3(dir), e1, e2 );
+
+    vec3 x = inverse(A) * b;
+
+    if(xChecker(x)){
+      cout << "(" << x.x << ", " << x.y << ", " << x.z << ")\n";
+
+    }
+
+    else cout << "You ruined it!";
+
+    // cout << "(" << x.x << ", " << x.y << ", " << x.z << ")\n";
+
+
+  }
+return false;
 }
